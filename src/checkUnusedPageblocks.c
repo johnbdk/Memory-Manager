2eba940f4206abc8ae434843f7d28cedd199edc8
margkeka <marrgkeka@gmail.com>
2019-05-25 14:06:44 +0300

    change the test file for unused pageblocks

--- src/checkUnusedPageblocks.c
+++ src/checkUnusedPageblocks.c
@@ -26,6 +26,16 @@ int main(int argc, char* argv[]){
 	for(int i = 0; i < 4*1024; i++)
 		my_free((void *) ad[i]);
 
+	printf("reallocate\n");
+	for (int i = 0; i < 4*1024; i++) {
+		ad[i] = (int *) my_malloc(2*sizeof(int)); //8 bytes -> slot 0
+		ad[i][0] = 99;
+		ad[i][1] = 782;
+		fflush(stdout);
+	}
+
+	printf("%d, %d --- %p\n", ad[0][0], ad[0][1], ad[0]);
+
 	//printf("my_free()\n");
 	//my_free((void *) ad2);
 
